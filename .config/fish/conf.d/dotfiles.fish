31152e8fd86606e6a9c1173b6baa7674af3a84b4
gkzhb <zhb896579388@163.com>
2021-03-17 23:21:34 +0800

    Add fish config with dotfiles alias and nvm env

---
+++ .config/fish/conf.d/dotfiles.fish
@@ -0,0 +1,2 @@
+alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+alias dv='env GIT_DIR=$HOME/.dotfiles nvim'
---
+++ .config/fish/conf.d/nvm-custom.fish
@@ -0,0 +1,5 @@
+export NVM_NODEJS_ORG_MIRROR=http://npm.taobao.org/mirrors/node
+export NVM_IOJS_ORG_MIRROR=http://npm.taobao.org/mirrors/iojs
+
+# set node default version
+set --universal nvm_default_version v12
