ef0be614b7f2f4a890406e26e9c9b8ea19c885be
gkzhb <zhb896579388@163.com>
2021-03-17 23:39:47 +0800

    Update dotfiles alias

--- .config/fish/conf.d/dotfiles.fish
+++ .config/fish/conf.d/dotfiles.fish
@@ -1,2 +1,2 @@
-alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+alias config='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 alias dv='env GIT_DIR=$HOME/.dotfiles nvim'
