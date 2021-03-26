12b891d97a1e982f74a1e1354ff5ab591886fc20
gkzhb <zhb896579388@163.com>
2021-03-26 22:30:39 +0800

    Add ignore untracked files in init script

--- script/dotfiles/init-after-clone.sh
+++ script/dotfiles/init-after-clone.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-git --git-dir=$HOME/.dotfiles --work-tree=$HOME config --local core.worktree $HOME
-git --git-dir=$HOME/.dotfiles --work-tree=$HOME config --unset core.bare
+config="git --git-dir=$HOME/.dotfiles --work-tree=$HOME"
+$config config --local status.showUntrackedFiles no
+$config config --local core.worktree $HOME
+$config config --unset core.bare
