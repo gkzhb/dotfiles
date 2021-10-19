af9e573e745130e1329f2416e633d73b8810aa51
gkzhb <zhb896579388@163.com>
2021-10-20 01:02:30 +0800

    fix(nvim): update fugivite init

--- .config/nvim/lua/plugins/fugitive.lua
+++ .config/nvim/lua/plugins/fugitive.lua
@@ -1,25 +1,27 @@
 local M = {}
 
-function M.init()
-end
-
+-- Pipe precommit log into a new buffer
+-- > from https://github.com/tpope/vim-fugitive/issues/1854
 function FugitiveAfterGit()
   if not vim.fn.exists('*FugitiveResult') then
     return
   end
   local result = vim.fn.FugitiveResult()
-  if not vim.fn.filereadable(result.file or '') or (result.args and result.args[0] or '') ~= 'commit' or not result.exit_status then
+  if not vim.fn.filereadable(result.file or '')
+      or (result.args and result.args[0] or '') ~= 'commit'
+      or not result.exit_status then
     return
   end
   vim.cmd([[Gsplit -]])
 end
 
-vim.cmd([[
-  augroup FugitiveRelated
-    autocmd!
-    autocmd User FugitiveChanged call v:lua.FugitiveAfterGit()
-  augroup END
-]])
+function M.init()
+  vim.cmd([[
+    augroup FugitiveRelated
+      autocmd!
+      autocmd User FugitiveChanged call v:lua.FugitiveAfterGit()
+    augroup END
+  ]])
+end
 
 return M
-
