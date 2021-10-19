d711af462ca588a3ec523effbecbb0a5de327d0f
gkzhb <zhb896579388@163.com>
2021-10-20 00:52:04 +0800

    feat(nvim): add fugitive config(open buffer to store commit result)

---
+++ .config/nvim/lua/plugins/fugitive.lua
@@ -0,0 +1,25 @@
+local M = {}
+
+function M.init()
+end
+
+function FugitiveAfterGit()
+  if not vim.fn.exists('*FugitiveResult') then
+    return
+  end
+  local result = vim.fn.FugitiveResult()
+  if not vim.fn.filereadable(result.file or '') or (result.args and result.args[0] or '') == 'commit' or not result.exit_status then
+    return
+  end
+  vim.cmd([[Gsplit -]])
+end
+
+vim.cmd([[
+  augroup FugitiveRelated
+    autocmd!
+    autocmd User FugitiveChanged call v:lua.FugitiveAfterGit()
+  augroup END
+]])
+
+return M
+
