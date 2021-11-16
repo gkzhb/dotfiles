f81fb13ee31462e6385a1499fd9bfc727a5898cd
gkzhb <zhb896579388@163.com>
2021-11-17 02:02:33 +0800

    feat(nvim): update session_manager

--- .config/nvim/lua/plugins/session-manager.lua
+++ .config/nvim/lua/plugins/session-manager.lua
@@ -2,7 +2,7 @@ local M = {}
 
 function M.init()
   require('session_manager').setup({
-    autoload_last_session = false,
+    autoload_mode = require('session_manager.config').AutoloadMode.CurrentDir,
   })
   require('telescope').load_extension('sessions')
 end
