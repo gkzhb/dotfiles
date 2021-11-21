43a526bdee6e0321eaa3f1ad10961dddb2eb2a55
gkzhb <zhb896579388@163.com>
2021-11-21 11:53:44 +0800

    fix(nvim): disable auto open session of current dir

--- .config/nvim/lua/plugins/session-manager.lua
+++ .config/nvim/lua/plugins/session-manager.lua
@@ -2,7 +2,7 @@ local M = {}
 
 function M.init()
   require('session_manager').setup({
-    autoload_mode = require('session_manager.config').AutoloadMode.CurrentDir,
+    autoload_mode = require('session_manager.config').AutoloadMode.Disabled,
   })
   require('telescope').load_extension('sessions')
 end
