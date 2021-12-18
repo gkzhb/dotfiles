2f75cd30b0f5de48a434ef55890450480e647f7f
gkzhb <zhb896579388@163.com>
2021-12-18 23:20:02 +0800

    feat(swiftbar): add yabai status bar icon

---
+++ scripts/swiftbar/yabai.1d.lua
@@ -0,0 +1,57 @@
+#!/usr/bin/env lua
+
+local function getCmdOutput(cmd)
+  local openPop = assert(io.popen(cmd))
+  local output = openPop:read('*all')
+  openPop:close()
+  return output
+end
+
+local visibleSpaceCsv = getCmdOutput('yabai -m query --spaces  | jq -r \'map(select((.visible == 1) or (.windows | length > 0)))[] | [.index, .label, .display, .visible] | @csv\'')
+-- print(visibleSpaceCsv)
+local display = tonumber(getCmdOutput('yabai -m query --displays --display | jq ".index"'))
+-- print(display)
+local function split(str, sep)
+  local ret = {}
+  for v in string.gmatch(str, '([^' .. sep .. ']*)') do
+    table.insert(ret, v)
+  end
+  return ret
+end
+local spaces = split(visibleSpaceCsv, '\n')
+local spaceInfo = {}
+local temp
+for _, space in pairs(spaces) do
+  if #space > 0 then
+    temp = split(space, ',') -- index, label, display, visible
+    table.insert(spaceInfo, temp)
+  end
+end
+local function getDisplayName(item, display)
+  local ret
+  if item[2] and #(item[2]) > 2 then
+    ret = string.sub(item[2], 2, -2) .. '(' .. item[1] .. ')'
+  else
+    ret = item[1]
+  end
+  if item[4] == '1' then
+    ret = '*' .. ret .. '*'
+    if display == tonumber(item[3]) then
+      ret = '' .. ret .. ''
+    end
+  end
+  return ret
+end
+local str = ''
+local currentDisplay = 0
+for _, item in pairs(spaceInfo) do
+  if currentDisplay ~= item[3] then
+    str = str .. ' '
+    currentDisplay = item[3]
+  end
+  str = str .. ' ' .. getDisplayName(item, display)
+end
+str = str .. '  | font="Hack Nerd Font Mono"'
+print(str)
+print('---')
+print('yabai spaces')
