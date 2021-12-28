ca4b1bbac7ef0bb4e0504a0b7d21c3e555a2424d
gkzhb <zhb896579388@163.com>
2021-12-29 00:11:34 +0800

    fix(nvim): add vim-win config

---
+++ .config/nvim/lua/plugins/vim-win.lua
@@ -0,0 +1,18 @@
+local M = {}
+
+function M.init()
+  vim.g.win_ext_command_map = {
+    q = 'Wquit',
+    w = 'w',
+    x = 'Win#exit',
+  }
+end
+
+function M.mappings()
+  local wk = require('which-key')
+  wk.register({
+    w = { '<cmd>call win#Win()<CR>', 'win mode'},
+  }, { prefix = '<leader>' })
+end
+
+return M
