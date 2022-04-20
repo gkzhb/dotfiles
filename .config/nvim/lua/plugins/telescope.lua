812a122b64dfe512f895a0acc6a2c34b726bc0e7
gkzhb <zhb896579388@163.com>
2022-04-20 14:36:20 +0800

    feat(nvim): telescope add keymaps for git and builtins

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -31,20 +31,35 @@ function M.mappings()
   wk.register({
     s = {
       name = 'telescope search',
+      b = { '<cmd>lua require("telescope.builtin").buffers()<cr>', 'buffers' },
       f = { '<cmd>lua require("telescope.builtin").find_files()<cr>', 'files' },
       g = { '<cmd>lua require("telescope.builtin").live_grep()<cr>', 'grep content' },
-      b = { '<cmd>lua require("telescope.builtin").buffers()<cr>', 'buffers' },
       h = { '<cmd>lua require("telescope.builtin").help_tags()<cr>', 'help tags' },
+      j = { '<cmd>lua require("telescope.builtin").jumplist()<cr>', 'jump list entries' },
+      k = { '<cmd>lua require("telescope.builtin").loclist()<cr>', 'location list' },
       l = { '<cmd>lua require("telescope.builtin").resume()<cr>', 'last view' },
+      -- git VCS pickers
+      v = {
+        name = 'git related',
+        c = { '<cmd>lua require("telescope.builtin").git_commits()<cr>', 'git commits' },
+        b = { '<cmd>lua require("telescope.builtin").git_bcommits()<cr>', 'buffer\'s git commits' },
+      },
 
       c = { '<cmd>lua require("telescope").extensions.cheatsheet.cheatsheet()<cr>', 'cheatsheet' },
-      p = { '<cmd>lua require("telescope").extensions.project.project{}<cr>', 'project' },
-      t = { '<cmd>lua require("telescope").extensions.tele_tabby.list()<cr>', 'current tabs' },
-      y = { '<cmd>lua require("telescope").extensions.neoclip.default()<cr>', 'clipboard' },
-      w = { '<cmd>lua require("telescope").extensions.tmux.windows({})<cr>', 'tmux win' },
-      s = { '<cmd>SessionManager load_session<cr>', 'sessions' },
-      r = { '<cmd>lua require("telescope").extensions.frecency.frecency()<cr>', 'recent files' },
       n = { '<cmd>lua require("telescope").extensions.notify.notify()<cr>', 'notification history' },
+      p = { '<cmd>lua require("telescope").extensions.project.project{}<cr>', 'project' },
+      r = { '<cmd>lua require("telescope").extensions.frecency.frecency()<cr>', 'recent files' },
+      s = { '<cmd>SessionManager load_session<cr>', 'sessions' },
+      t = { '<cmd>lua require("telescope").extensions.tele_tabby.list()<cr>', 'current tabs' },
+      w = { '<cmd>lua require("telescope").extensions.tmux.windows({})<cr>', 'tmux win' },
+      y = { '<cmd>lua require("telescope").extensions.neoclip.default()<cr>', 'clipboard' },
+
+      -- deep keymaps, default telescope pickers
+      d = {
+        b = { '<cmd>lua require("telescope.builtin").builtin()<cr>', 'builtin pickers' },
+        p = { '<cmd>lua require("telescope.builtin").pickers()<cr>', 'previous pickers' },
+        r = { '<cmd>lua require("telescope.builtin").registers()<cr>', 'list vim registers' },
+      },
     },
   }, { prefix = '<leader>' })
 end
