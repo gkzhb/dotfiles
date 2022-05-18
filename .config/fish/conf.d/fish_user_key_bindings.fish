7d924bec11986083c3afd9512fab7e6defc1bad3
gkzhb <zhb896579388@163.com>
2022-05-18 17:46:53 +0800

    feat(fish): fix \\I\\O output

---
+++ .config/fish/conf.d/fish_user_key_bindings.fish
@@ -0,0 +1,8 @@
+function fish_user_key_bindings
+  fish_vi_key_bindings
+
+  # Fix extra '\\I\\O' output when stopping running commands
+  # Ref: https://github.com/fish-shell/fish-shell/issues/1917#issuecomment-72164812
+  bind \e\[I 'begin;end'
+  bind \e\[O 'begin;end'
+end
