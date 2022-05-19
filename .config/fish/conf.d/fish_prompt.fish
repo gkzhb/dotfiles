e5877c2d21caa5efa71db48a038360f1f1c1c275
gkzhb <zhb896579388@163.com>
2022-05-19 22:18:11 +0800

    feat(fish): prompt add cmd duration

--- .config/fish/conf.d/fish_prompt.fish
+++ .config/fish/conf.d/fish_prompt.fish
@@ -24,6 +24,7 @@ function fish_prompt
   set -l bred (set_color -o red)
   set -l bcyan (set_color -o cyan)
   set -l bwhite (set_color -o white)
+  set -l gray (set_color 555)
 
   # Configure __fish_git_prompt
   set -g __fish_git_prompt_show_informative_status true
@@ -31,15 +32,21 @@ function fish_prompt
 
   # Color prompt char red for non-zero exit status
   set -l pcolor $bpurple
-  if [ $last_status -ne 0 ]
+  if test $last_status -ne 0
     set pcolor $bred
   end
 
   # Top
   echo -n $cyan$USER$normal at $yellow$__fish_prompt_hostname$normal in $bred(prompt_pwd)$normal
   __fish_git_prompt
+  if test $last_status -ne 0
+    echo -n ' '$pcolor'('$last_status')'
+  end
+  if test $CMD_DURATION -ne 0
+    echo -n ' '$gray'('(__fish_prompt_get_duration)')'
+  end
 
-  echo
+  echo $normal
 
   # Bottom
   echo -n $pcolor$__fish_prompt_char $normal
@@ -47,18 +54,29 @@ end
 
 # from https://github.com/yeseni-today/ays-fish-theme/blob/master/fish_right_prompt.fish
 function fish_right_prompt
-  # set -l exit_code $status
   __lf_prompt
   __tmux_prompt
-  # if test $exit_code -ne 0
-  #   set_color -o red
-  # else
-  #   set_color 666666
-  # end
-  # printf '%d' $exit_code
   set_color 666666
   printf ' [%s]' (date +%H:%M:%S)
   set_color normal
+end
+
+
+# show command execution duration, from https://github.com/rideron89/rider-theme/blob/master/fish_right_prompt.fish
+function __fish_prompt_get_duration
+  set -l seconds (math "$CMD_DURATION / 1000")
+  set -l minutes (math "$seconds / 60")
+  set -l hours (math "$minutes / 60")
+
+  if test $hours -gt 0.1
+    echo "$hours hrs"
+  else if test $minutes -gt 1
+    echo "$minutes mins"
+  else if test $seconds -gt 0.1
+    echo "$seconds s"
+  else
+    echo "$CMD_DURATION ms"
+  end
 end
 
 function __tmux_prompt
@@ -75,7 +93,7 @@ function __tmux_prompt
   if test -z $pane
     echo -n ""
   else
-    echo -n $pane' | '
+    echo -n $pane' |'
   end
 end
 
@@ -84,7 +102,7 @@ function __lf_prompt
   if test -z $LF_LEVEL
     echo -n ''
   else
-    echo -n 'lf: '$LF_LEVEL' | '
+    echo -n 'lf: '$LF_LEVEL' |'
   end
 end
 
