bfb7cef7719cfffe523e2370910fe34d90ea9aa2
gkzhb <gkzhb98@gmail.com>
2024-06-04 16:40:02 +0800

    chore(fish): update nvm mirror url

--- .config/fish/conf.d/common.fish
+++ .config/fish/conf.d/common.fish
@@ -60,7 +60,7 @@ alias g=git
 
 # {{{1 nvm
 # set node download mirror
-set -uU nvm_mirror http://npm.taobao.org/mirrors/node
+set -uU nvm_mirror https://npmmirror.com/mirrors/node/
 # set node default version
 # set -uU nvm_default_version v12
 # {{{1 npm registry
