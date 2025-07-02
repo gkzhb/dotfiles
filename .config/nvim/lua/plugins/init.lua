351b250781818295087df33a1319c88abc15a557
gkzhb <gkzhb98@gmail.com>
2025-07-02 19:59:01 +0800

    feat(nvim): add tmux navigator plugin

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -168,6 +168,16 @@ require('pckr').add({
   },
   'szw/vim-maximizer',
   'tpope/vim-unimpaired', -- `[<char>`, `]<char>` related keymappings
+  {
+    'numToStr/Navigator.nvim',
+    config = function()
+      require('Navigator').setup()
+      vim.keymap.set({ 'n', 't' }, '<C-h>', '<CMD>NavigatorLeft<CR>')
+      vim.keymap.set({ 'n', 't' }, '<C-l>', '<CMD>NavigatorRight<CR>')
+      vim.keymap.set({ 'n', 't' }, '<C-k>', '<CMD>NavigatorUp<CR>')
+      vim.keymap.set({ 'n', 't' }, '<C-j>', '<CMD>NavigatorDown<CR>')
+    end
+  },
   -- {{{2 edit
   'tpope/vim-surround',
   {
