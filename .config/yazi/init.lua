8f984b0f4ce80e00c02b4b122ea97e41d2e27f6d
gkzhb <gkzhb98@gmail.com>
2025-07-20 17:58:19 +0800

    feat(yazi): add git plugin

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -2,6 +2,8 @@ require("zoxide"):setup {
 	update_db = true,
 }
 
+require("git"):setup()
+
 require("relative-motions"):setup({ show_numbers="relative_absolute", show_motion = true, enter_mode ="first" })
 
 -- require("githead"):setup({
