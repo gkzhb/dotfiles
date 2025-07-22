9c3617afd1ce05f828ce8b85edaf8167d02454b1
gkzhb <gkzhb98@gmail.com>
2025-07-22 16:06:05 +0800

    feat: update kitty shell

--- scripts/yabai/open-kitty.sh
+++ scripts/yabai/open-kitty.sh
@@ -3,10 +3,10 @@
 # Used by yabai and skhd
 # Open kitty window in the current display instead of the display where the
 # first kitty window was created.
-index=$(yabai -m query --displays --display | jq .index)
-yabai -m signal --add event=window_created action=" \
-  yabai -m signal --remove 'testkitty' &&
-  yabai -m window $YABAI_WINDOW_ID --display $index &&
-  yabai -m display --focus $index" \
-app="kitty" label="testkitty"
+# index=$(yabai -m query --displays --display | jq .index)
+# yabai -m signal --add event=window_created action=" \
+#   yabai -m signal --remove 'testkitty' &&
+#   yabai -m window $YABAI_WINDOW_ID --display $index &&
+#   yabai -m display --focus $index" \
+# app="kitty" label="testkitty"
 kitty --single-instance --instance-group=1 -d ~
