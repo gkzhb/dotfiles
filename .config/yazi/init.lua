50d0ecf04686fb608e50adfc91e89a1b1496e064
gkzhb <gkzhb98@gmail.com>
2025-08-13 02:33:36 +0800

    feat(yazi): add yatline and its githead

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -1,52 +1,99 @@
 require("zoxide"):setup {
-	update_db = true,
+  update_db = true,
 }
 
 require("git"):setup()
 
-require("relative-motions"):setup({ show_numbers="relative_absolute", show_motion = true, enter_mode ="first" })
+require("relative-motions"):setup({ show_numbers = "relative_absolute", show_motion = true, enter_mode = "first" })
 
--- require("githead"):setup({
---   branch_color = "",
---   branch_prefix = "on",
---   branch_symbol = " ",
---   branch_borders = "()",
---   show_numbers = false,
---   show_remote_branch = false,
---   show_tag = false,
---   show_commit = false,
---   show_behind_ahead_remote = false,
---   show_stashes = false,
---   show_state = false,
---   show_staged = false,
---   show_unstaged = false,
---   show_untracked = false,
+require("yatline"):setup({
+  show_background = true,
 
---   remote_branch_prefix = "@",
---   remote_branch_color = "",
+  header_line = {
+    left = {
+      section_a = {
+        { type = "string", custom = false, name = "tab_path" },
+      },
+      section_b = {
+      },
+      section_c = {
+        { type = "coloreds", custom = false, name = "githead" },
+      }
+    },
+    right = {
+      section_a = {
+        { type = "line", custom = false, name = "tabs", params = { "right" } },
+        -- { type = "coloreds", custom = true, name = { { " 󰇥 ", "#3c3836" } } },
+      },
+      section_b = {
+      },
+      section_c = {
+        { type = "coloreds", custom = false, name = "count" },
+      }
+    }
+  },
 
---   tag_color = "",
+  status_line = {
+    left = {
+      section_a = {
+      },
+      section_b = {
+      },
+      section_c = {
+      }
+    },
+    right = {
+      section_a = {
+      },
+      section_b = {
+      },
+      section_c = {
+      }
+    }
+  },
+})
 
---   commit_color = "",
+require("yatline-githead"):setup({
+  branch_color = "",
+  branch_prefix = "on",
+  branch_symbol = " ",
+  branch_borders = "()",
+  show_numbers = false,
+  show_remote_branch = false,
+  show_tag = false,
+  show_commit = false,
+  show_behind_ahead_remote = false,
+  show_stashes = false,
+  show_state = false,
+  show_staged = false,
+  show_unstaged = false,
+  show_untracked = false,
 
---   ahead_remote_symbol = "+",
---   ahead_remote_color = "",
---   behind_remote_symbol = "-",
---   behind_remote_color = "",
+  remote_branch_prefix = "@",
+  remote_branch_color = "",
 
---   stashes_symbol = "*",
---   stashes_color = "",
+  tag_color = "",
 
---   show_state_prefix = false,
---   state_symbol = "~",
---   state_color = "",
+  commit_color = "",
 
---   staged_symbol = "S",
---   staged_color = "",
+  ahead_remote_symbol = "+",
+  ahead_remote_color = "",
+  behind_remote_symbol = "-",
+  behind_remote_color = "",
 
---   unstaged_symbol = "U",
---   unstaged_color = "",
+  stashes_symbol = "*",
+  stashes_color = "",
 
---   untracked_symbol = "N",
---   untracked_color = "",
--- })
+  show_state_prefix = false,
+  state_symbol = "~",
+  state_color = "",
+
+  staged_symbol = "S",
+  staged_color = "",
+
+  unstaged_symbol = "U",
+  unstaged_color = "",
+
+  untracked_symbol = "N",
+  untracked_color = "",
+})
