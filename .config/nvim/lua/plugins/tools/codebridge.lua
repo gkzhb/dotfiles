d3f17cbc1ca6b8662eabea2b92694b0ab8a767e9
gkzhb <gkzhb98@gmail.com>
2026-01-16 20:52:42 +0800

    fix(tmux): add bind to send prefix

--- .config/nvim/lua/plugins/tools/codebridge.lua
+++ .config/nvim/lua/plugins/tools/codebridge.lua
@@ -3,16 +3,16 @@ local M = {
   opts = {
     tmux = {
       target_mode = 'find_process', -- 'window_name', 'current_window', 'find_process'
-      window_name = 'coco', -- window name to search for when target_mode = 'window_name'
-      process_name = 'coco', -- process name(s) to search for when target_mode = 'current_window' or 'find_process'
-      switch_to_target = true, -- whether to switch to the target after sending
-      find_node_process = false, -- whether to look for node processes with matching name
+      window_name = 'coco',         -- window name to search for when target_mode = 'window_name'
+      process_name = 'coco',        -- process name(s) to search for when target_mode = 'current_window' or 'find_process'
+      switch_to_target = true,      -- whether to switch to the target after sending
+      find_node_process = false,    -- whether to look for node processes with matching name
     },
   },
   keys = {
-    { '<leader>ca', '<cmd>CodeBridgeTmux<CR>', desc = 'Send to Coco' },
-    { '<leader>ci', '<cmd>CodeBridgeTmuxInteractive<CR>', desc = 'Send to Coco' },
-    { '<leader>cd', '<cmd>CodeBridgeTmuxDiff<CR>', desc = 'Send diff to Coco' },
+    { '<leader>ca', '<cmd>CodeBridgeTmux<CR>',            desc = 'Send to Coco' },
+    { '<leader>ci', '<cmd>CodeBridgeTmuxInteractive<CR>', desc = 'Send to Coco',     mode = { 'n', 'v' } },
+    { '<leader>cd', '<cmd>CodeBridgeTmuxDiff<CR>',        desc = 'Send diff to Coco' },
   },
 }
 return M
