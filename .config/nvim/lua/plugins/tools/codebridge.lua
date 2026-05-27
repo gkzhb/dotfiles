3b1fc1ea37334c0fe1bc77afabf8b39db0f370cf
gkzhb <gkzhb98@gmail.com>
2026-05-27 22:01:56 +0800

    chore(nvim): update config

--- .config/nvim/lua/plugins/tools/codebridge.lua
+++ .config/nvim/lua/plugins/tools/codebridge.lua
@@ -3,8 +3,8 @@ local M = {
   opts = {
     tmux = {
       target_mode = 'find_process', -- 'window_name', 'current_window', 'find_process'
-      window_name = 'coco', -- window name to search for when target_mode = 'window_name'
-      process_name = 'coco', -- process name(s) to search for when target_mode = 'current_window' or 'find_process'
+      window_name = 'claude', -- window name to search for when target_mode = 'window_name'
+      process_name = 'claude', -- process name(s) to search for when target_mode = 'current_window' or 'find_process'
       switch_to_target = false, -- whether to switch to the target after sending
       find_node_process = false, -- whether to look for node processes with matching name
     },
