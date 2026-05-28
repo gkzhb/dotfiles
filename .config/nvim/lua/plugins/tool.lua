95e68f9122063e9f92fdd722875270b3a69d5ce5
gkzhb <gkzhb98@gmail.com>
2026-05-28 16:49:45 +0800

    chore: disable telescope fzf plugin

--- .config/nvim/lua/plugins/tool.lua
+++ .config/nvim/lua/plugins/tool.lua
@@ -33,14 +33,14 @@ return {
     'Marskey/telescope-sg',
   },
 
-  {
-    'nvim-telescope/telescope-fzf-native.nvim',
-    build = 'make',
-    dependencies = { 'nvim-telescope/telescope.nvim' },
-    config = function()
-      require('telescope').load_extension('fzf')
-    end,
-  },
+  -- {
+  --   'nvim-telescope/telescope-fzf-native.nvim',
+  --   build = 'make',
+  --   dependencies = { 'nvim-telescope/telescope.nvim' },
+  --   config = function()
+  --     require('telescope').load_extension('fzf')
+  --   end,
+  -- },
   {
     "alexpasmantier/tv.nvim",
   },
