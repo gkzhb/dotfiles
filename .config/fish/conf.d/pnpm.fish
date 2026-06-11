e7c1a7a59248134023dc34dcbb064f024eea002f
gkzhb <gkzhb98@gmail.com>
2026-06-11 13:23:09 +0800

    fix(fish): update pnpm bin path

--- .config/fish/conf.d/pnpm.fish
+++ .config/fish/conf.d/pnpm.fish
@@ -1,4 +1,10 @@
 set -gx PNPM_HOME "$HOME/.local/share/pnpm"
-if not string match -q -- $PNPM_HOME $PATH
-  fish_add_path $PNPM_HOME
+
+if test (uname) = Darwin
+  set -l PNPM_BIN_PATH $PNPM_HOME
+else
+  set -l PNPM_BIN_PATH $PNPM_HOME/bin
+end
+if not string match -q -- $PNPM_BIN_PATH $PATH
+  fish_add_path $PNPM_BIN_PATH
 end
